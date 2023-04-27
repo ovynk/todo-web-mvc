7feabf859c3c12dc1c46a016cae4adffd32614aa
Oleksiy <vinokurovlosa@gmail.com>
2023-04-27 19:16:20 +0300

    Implement Absolute global chat in each todo(sample)

---
+++ src/main/java/com/oleksiy/todo/controller/ChatController.java
@@ -0,0 +1,33 @@
+package com.oleksiy.todo.controller;
+
+import com.oleksiy.todo.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public/")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        simpMessagingTemplate.convertAndSend("/topic/public", chatMessage);
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
--- src/main/java/com/oleksiy/todo/security/WebSecurityConfig.java
+++ src/main/java/com/oleksiy/todo/security/WebSecurityConfig.java
@@ -2,13 +2,9 @@ package com.oleksiy.todo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
